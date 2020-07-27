e52af35cca793093b65d3900d995fa3a89318ee0
Imran Ahmed <imran.ahmed@qbridgeplatform.com>
2020-07-27 16:49:21 +0100

    Testing ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import 'reflect-metadata';
 import '../polyfills';
+import 'ag-grid-enterprise';
+import 'reflect-metadata';
 
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-
-import { AppRoutingModule } from './app-routing.module';
-
-// NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { HomeModule } from './home/home.module';
-import { DetailModule } from './detail/detail.module';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DetailModule } from './detail/detail.module';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
+// NG Translate
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,4 +4,16 @@ import { Component } from '@angular/core';
     selector: 'app-home',
     templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    columnDefs = [
+        { headerName: 'Make', field: 'make' },
+        { headerName: 'Model', field: 'model' },
+        { headerName: 'Price', field: 'price' },
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 },
+    ];
+}
